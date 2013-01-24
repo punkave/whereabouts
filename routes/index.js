b8027b61b2a95104af187ad67a1f4eb557d98d40
Tom Boutell <tom@punkave.com>
2013-01-24 12:05:18 -0500

    * Login via gmail authentication * gmail is the system of record - not us. So if we see a new user logged in, just create them in mongodb * A bunch of small fixes in the client side code (callbacks, you gotta have 'em)

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var dbConfig = require("../config/dbConfig.js");
 var db = require("mongojs").connect(dbConfig.databaseUrl, dbConfig.collections);
 
 exports.index = function(req, res) {
+  console.log(req.user);
   db.punks.find(function(err, docs) {
-    res.render('index', { title: 'Punks', punks: docs, statuses: dbConfig.hardStatuses });
+    res.render('index', { title: 'Punks', me: req.user, punks: docs, statuses: dbConfig.hardStatuses });
   });
 }
